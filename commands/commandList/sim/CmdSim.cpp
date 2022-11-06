2628c26340601cad48f5da66a06b7a0e9a757777
quimmilho <jrodmilheiro@gmail.com>
2022-11-06 18:55:36 +0000

    Implementada a função de execução dos comandos slide, exit, exit

---
+++ commands/commandList/sim/CmdSim.cpp
@@ -0,0 +1,61 @@
+#include "CmdExit.h"
+#include "CmdN.h"
+#include "../../../handler/Handler.h"
+#include "../../../exceptions/OutOfBounds.h"
+#include "../../../exceptions/InvalidArguments.h"
+#include "CmdSlide.h"
+#include "../../../exceptions/NotANumber.h"
+
+namespace tppoo {
+
+    int CmdExit::execute(std::string cmd, std::string *args, int nargs) {
+        return 1;
+    }
+
+    int CmdN::execute(std::string cmd, std::string *args, int nargs) {
+        int n, p;
+        switch (nargs) {
+            case 0:
+                Handler::instance->getSimulation()->tick();
+                Handler::instance->getSimulation()->render();
+                break;
+            case 1:
+                n = toNumber(args[0]);
+                if (n <= 0) throw OutOfBounds();
+                Handler::instance->getSimulation()->tickMultiple(n);
+                break;
+            case 2:
+                n = toNumber(args[0]);
+                p = toNumber(args[1]);
+                if (n <= 0 || p <= 0) throw OutOfBounds();
+                Handler::instance->getSimulation()->tickMultiple(n, p);
+                break;
+            default:
+                throw InvalidArguments();
+        }
+        return 0;
+    }
+
+    int CmdSlide::execute(std::string cmd, std::string *args, int nargs) {
+        if (nargs != 2) {
+            throw InvalidArguments();
+        }
+        int n = toNumber(args[1]);
+        if (isNumber(args[0])) {
+            Handler::instance->getSimulation()->addOffset(toNumber(args[0]), n);
+            return 0;
+        }
+        if (n <= 0) throw OutOfBounds();
+        if (args[0] == "up") {
+            Handler::instance->getSimulation()->addYOffset(-n);
+        } else if (args[0] == "down") {
+            Handler::instance->getSimulation()->addYOffset(n);
+        } else if (args[0] == "right") {
+            Handler::instance->getSimulation()->addXOffset(n);
+        } else if (args[0] == "left") {
+            Handler::instance->getSimulation()->addXOffset(-n);
+        } else throw InvalidArguments();
+        return 0;
+    }
+
+}
