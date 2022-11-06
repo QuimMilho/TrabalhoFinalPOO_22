5c90b40e867d77a685304b4bec52a5a6e066b6b3
quimmilho <jrodmilheiro@gmail.com>
2022-11-06 12:36:27 +0000

    Comando exit implementado!

--- commands/commandList/sim/CmdExit.h
+++ commands/commandList/sim/CmdExit.h
@@ -1,10 +1,15 @@
 #ifndef TRABALHOFINALPOO_22_CMDEXIT_H
 #define TRABALHOFINALPOO_22_CMDEXIT_H
 
+#include "../../Command.h"
+
 namespace tppoo {
 
-    class CmdExit {
-
+    class CmdExit : public Command {
+    public:
+        int execute(std::string cmd, std::string* args, int nargs) override {
+            return 1;
+        }
     };
 
 }
