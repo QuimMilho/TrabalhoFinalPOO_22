a405bac3da096f63b2747b8c9ab2baf5ab39434e
quimmilho <jrodmilheiro@gmail.com>
2022-11-08 01:54:44 +0000

    Adicionado comando para criar uma nova simulaçao

---
+++ commands/commandList/sim/CmdNew.hpp
@@ -0,0 +1,14 @@
+#ifndef TRABALHOFINALPOO_22_CMDNEW_HPP
+#define TRABALHOFINALPOO_22_CMDNEW_HPP
+
+#include "../../Command.hpp"
+
+namespace tppoo {
+
+    class CmdNew : public Command {
+        int execute(std::string cmd, std::string *args, int nargs) override;
+    };
+
+}
+
+#endif
