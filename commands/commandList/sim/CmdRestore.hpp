ee038898e515d693ae55a7d2bd93667a80d8293d
quimmilho <jrodmilheiro@gmail.com>
2022-11-08 01:55:11 +0000

    Implementado comando para restaurar uma simulação guardada

--- commands/commandList/sim/CmdRestore.hpp
+++ commands/commandList/sim/CmdRestore.hpp
@@ -1,10 +1,12 @@
 #ifndef TRABALHOFINALPOO_22_CMDRESTORE_HPP
 #define TRABALHOFINALPOO_22_CMDRESTORE_HPP
 
+#include "../../Command.hpp"
+
 namespace tppoo {
 
-    class CmdRestore {
-
+    class CmdRestore : public Command {
+        int execute(std::string cmd, std::string *args, int nargs) override;
     };
 
 }
