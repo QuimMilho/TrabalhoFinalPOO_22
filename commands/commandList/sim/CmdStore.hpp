50636e0f31cff829c5a4f603e8b0d1285ca5b55b
quimmilho <jrodmilheiro@gmail.com>
2022-11-08 01:55:24 +0000

    Implementado comando para guardar uma simulação

--- commands/commandList/sim/CmdStore.hpp
+++ commands/commandList/sim/CmdStore.hpp
@@ -1,10 +1,12 @@
 #ifndef TRABALHOFINALPOO_22_CMDSTORE_HPP
 #define TRABALHOFINALPOO_22_CMDSTORE_HPP
 
+#include "../../Command.hpp"
+
 namespace tppoo {
 
-    class CmdStore {
-
+    class CmdStore : public Command {
+        int execute(std::string cmd, std::string *args, int nargs) override;
     };
 
 }
