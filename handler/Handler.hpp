70abb5d1cd6382b923d7b25f46e69daa58083e2b
quimmilho <jrodmilheiro@gmail.com>
2022-11-28 14:29:17 +0000

    Criada função para dar update às coordenadas e às barreiras da área visível. Criadas as janelas do trabalho presentes na consola e definidas constantes relativas à posição da barreira

--- handler/Handler.hpp
+++ handler/Handler.hpp
@@ -6,6 +6,11 @@
 #include "../utils/utils.hpp"
 #include "../utils/Terminal.h"
 
+#define TOP 0
+#define BOTTOM 2
+#define LEFT 1
+#define RIGHT 3
+
 namespace tppoo {
 
     class Handler {
@@ -16,11 +21,34 @@ namespace tppoo {
         std::unordered_map<std::string, Simulation *> *simulations;
         Random *rand;
         std::string active;
-        term::Terminal term;
-        term::Window simulationWindow;
-        term::Window historyWindow;
-        term::Window commandResponseWindow;
+        term::Terminal &term = term::Terminal::instance();
+        // Simulation coords
+        term::Window topCoordsWindow = term.create_window(2, 1, 7, 1, false);
+        term::Window botCoordsWindow = Simulation::getNC() < MAX_X_SIZE ?
+                term.create_window(MAX_X_SIZE - 1 - (MAX_X_SIZE - Simulation::getNC()), 19, 7, 1, false) :
+                term.create_window(52, 19, 7, 1, false);
+
+        // Simulation Borders
+        term::Window topBorderWindow = Simulation::getNC() < MAX_X_SIZE ?
+                term.create_window(2,2,MAX_X_SIZE - (MAX_X_SIZE - Simulation::getNC()) + 4,1, false)
+                : term.create_window(2,2,57,1, false);
+        term::Window botBorderWindow = Simulation::getNC() < MAX_X_SIZE ?
+                term.create_window(2, 18,MAX_X_SIZE - (MAX_X_SIZE - Simulation::getNC()) + 4,1, false)
+                : term.create_window(2,18,57,1, false);
+        term::Window rightBorderWindow = Simulation::getNC() < MAX_X_SIZE ?
+                term.create_window(MAX_X_SIZE - (MAX_X_SIZE - Simulation::getNC()) + 4, 3, 2, 15, false)
+                : term.create_window(57,3,2,15, false);
+        term::Window leftBorderWindow = term.create_window(2,3,2,15, false);
     public:
+        // Windows
+        term::Window simulationWindow = Simulation::getNC() < MAX_X_SIZE ?
+                term.create_window(4, 3, MAX_X_SIZE - (MAX_X_SIZE - Simulation::getNC()), MAX_Y_SIZE, false)
+                : term.create_window(4, 3, 53, 15, false);
+        term::Window historyWindow = Simulation::getNC() < MAX_X_SIZE ?
+                term.create_window(61 - (MAX_X_SIZE - Simulation::getNC()), 1, 57 + (MAX_X_SIZE - Simulation::getNC()), 19)
+                : term.create_window(61, 1, 57, 19);
+        term::Window commandWindow = term.create_window(2, 21, 116, 8);
+
         static Handler* instance;
         Handler();
         ~Handler();
@@ -37,6 +65,8 @@ namespace tppoo {
         void copySim(Simulation *saved);
         void copySim(Simulation *saved, Simulation *newSim);
         bool simulationExists(const std::string& name);
+        void updateCoords();
+        void updateBorder(int b, char c);
     };
 
 }
